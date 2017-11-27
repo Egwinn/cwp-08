78c1bc41db0da06777c16b9762f3d9bd67e473e4
Marina <dubovik.m14@gmail.com>
2017-11-27 11:09:36 +0200

    add changes to worker.js

--- worker.js
+++ worker.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 
 let filepath = process.argv[2];
 let interval = process.argv[3];
+let min = 0;
+let max = 100;
 
 setInterval(function() {
     fs.appendFile(filepath, 
         fs.existsSync(filepath) ?
-        "," + JSON.stringify(Math.round(Math.random() * 100), null, "\t") :
-        "[" + JSON.stringify(Math.round(Math.random() * 100), null, "\t"), 
+        "," + JSON.stringify(Math.round(Math.random() * (max - min)) + min, null, "\t") :
+        "[" + JSON.stringify(Math.round(Math.random() * (max - min)) + min, null, "\t"), 
         (err) => {
             if(err) console.error(err);
         });
